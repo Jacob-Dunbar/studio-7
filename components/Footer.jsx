60b5c74240638586ed86be95b11374f7ca52a985
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-10 19:13:09 +0000

    feat: add layout with navbar and footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className=" flex flex-col items-center">
+      <p>2022 Studio 7 All rights reserved</p>
+      <p className="flex gap-2">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex px-4 justify-between">
+      <p>
+        <Link href="/">Studio 7</Link>
+      </p>
+
+      <button className=" relative" type="button" onClick>
+        <AiOutlineShopping />
+        <span className=" absolute top-0 bg-red-600 rounded-lg text-xs text-white w-4 h-4">
+          1
+        </span>
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
