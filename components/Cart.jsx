d77677d33b5ca42ac98e5ba27296a417fa09decc
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-11 17:06:53 +0000

    feat: add payment flow with stripe

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -10,10 +10,32 @@ import { TiDeleteOutline } from "react-icons/ti";
 import toast from "react-hot-toast";
 import { useStateContext } from "../context/StateContext";
 import { urlFor } from "../lib/client";
+import getStripe from "../lib/getStripe";
 
 const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, cartItems, setShowCart, onRemove } = useStateContext();
+
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+    const response = await fetch("/api/stripe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if (response.statusCode === 500) return;
+
+    const data = await response.json();
+
+    toast.loading("Redirecting...");
+
+    stripe.redirectToCheckout({ sessionId: data.id });
+  };
+
   return (
     <div className="fixed right-0 z-10 h-screen bg-red-500 " ref={cartRef}>
       <div>
@@ -38,14 +60,10 @@ const Cart = () => {
         <div>
           {cartItems.length >= 1 &&
             cartItems.map((item) => (
-              <div key={item.product._id}>
-                <img
-                  className=" w-28"
-                  src={urlFor(item.product.image[0])}
-                  alt=""
-                />
-                <h5>{item.product.name}</h5>
-                <h4>£{item.product.price}</h4>
+              <div key={item._id}>
+                <img className=" w-28" src={urlFor(item.image[0])} alt="" />
+                <h5>{item.name}</h5>
+                <h4>£{item.price}</h4>
                 <div>
                   <button onClick={() => onRemove(item)} type="button">
                     <TiDeleteOutline />
@@ -58,7 +76,9 @@ const Cart = () => {
           <div>
             <h3>Sub total</h3>
             <h3>£{totalPrice}</h3>{" "}
-            <button type="button">Pay with Stripe</button>
+            <button type="button" onClick={handleCheckout}>
+              Pay with Stripe
+            </button>
           </div>
         )}
       </div>
---
+++ pages/api/stripe.js
@@ -0,0 +1,51 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    console.log(req.body);
+    try {
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/22tuq0nq/production/"
+            )
+            .replace("-webp", ".webp");
+          return {
+            price_data: {
+              currency: "gbp",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: false,
+            },
+            quantity: 1,
+          };
+        }),
+
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      };
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.status(200).json(session);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
