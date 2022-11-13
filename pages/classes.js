9a0a0c6769179b645a2ef89a0dbc9f0b571da878
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-13 18:25:01 +0000

    feat: add search feature

--- pages/classes.js
+++ pages/classes.js
@@ -1,44 +1,77 @@
 import Product from "../components/Product";
 import { client } from "../lib/client";
 import Link from "next/link";
+import { useState } from "react";
 
-// export const getServerSideProps = async () => {
-//   //   const query = '*[_type == "class"]';
-// //   const query = '*[[details, name, catagories] match ["fighting"]]';
-//   const products = await client.fetch(query);
+export const getServerSideProps = async () => {
+  const query = '*[_type == "class"]';
+  //   const query = '*[[details, name, catagories] match ["fighting"]]';
+  const products = await client.fetch(query);
 
-//   const bannerQuery = '*[_type == "banner"]';
-//   const bannerData = await client.fetch(bannerQuery);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
 
-//   return {
-//     props: { products, bannerData },
-//   };
-// };
+  return {
+    props: { products, bannerData },
+  };
+};
 
 const Classes = ({ products, bannerData }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  console.log(products);
   return (
     <div className="py-4 ">
+      <input
+        type="text"
+        placeholder="search"
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
       <div className="button"> search </div>
-      <div className="flex flex-col items-center gap-5 mt-6 text-2xl">
-        <Link href="/catagories/mindfulness">
-          <h2 className="w-full py-10 text-center bg-slate-400">Mindfulness</h2>
-        </Link>
-        <Link href="/catagories/strength">
-          <h2 className="w-full py-10 text-center bg-slate-400">Strength</h2>
-        </Link>
-        <Link href="/catagories/mobility">
-          <h2 className="w-full py-10 text-center bg-slate-400">Mobilty</h2>
-        </Link>
-        <Link href="/catagories/cardio">
-          <h2 className="w-full py-10 text-center bg-slate-400">Cardio</h2>
-        </Link>
-        <Link href="/catagories/combat">
-          <h2 className="w-full py-10 text-center bg-slate-400">Combat</h2>
-        </Link>
-        <Link href="/catagories/all">
-          <h2 className="w-full py-10 text-center bg-slate-400">All</h2>
-        </Link>
-      </div>
+      {!searchTerm ? (
+        <div className="flex flex-col items-center gap-5 mt-6 text-2xl">
+          <Link href="/catagories/mindfulness">
+            <h2 className="w-full py-10 text-center bg-slate-400">
+              Mindfulness
+            </h2>
+          </Link>
+          <Link href="/catagories/strength">
+            <h2 className="w-full py-10 text-center bg-slate-400">Strength</h2>
+          </Link>
+          <Link href="/catagories/mobility">
+            <h2 className="w-full py-10 text-center bg-slate-400">Mobilty</h2>
+          </Link>
+          <Link href="/catagories/cardio">
+            <h2 className="w-full py-10 text-center bg-slate-400">Cardio</h2>
+          </Link>
+          <Link href="/catagories/combat">
+            <h2 className="w-full py-10 text-center bg-slate-400">Combat</h2>
+          </Link>
+          <Link href="/catagories/all">
+            <h2 className="w-full py-10 text-center bg-slate-400">All</h2>
+          </Link>
+        </div>
+      ) : (
+        products
+          .filter((product) => {
+            if (
+              product.details.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return product;
+            } else if (
+              product.name.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return product;
+            } else if (
+              product.catagories[0]
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase())
+            ) {
+              return product;
+            }
+          })
+          .map((product) => <Product product={product} />)
+      )}
     </div>
   );
 };
