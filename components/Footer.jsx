1d657366aad6d47e68b35f49bbeb44550f7e7ce3
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-17 19:45:08 +0000

    style: add foot styles

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,12 +3,12 @@ import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center ">
-      <p>2022 Studio 7 All rights reserved</p>
+    <div className="flex flex-col items-center justify-center gap-1 tems-center ">
       <p className="flex gap-2">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+        <AiFillInstagram className="w-8 h-8" />
+        <AiOutlineTwitter className="w-8 h-8" />
       </p>
+      <p>2022 Studio 7 All rights reserved</p>
     </div>
   );
 };
