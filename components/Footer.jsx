1e8e20bc6b73f8d3c829d2c15d426ad3ae98d4df
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-20 16:49:50 +0000

    style: desktop styles footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-1 tems-center ">
+    <div className="flex flex-col items-center justify-center gap-1 sm:text-sm tems-center ">
       <p>2022 Studio 7 All rights reserved</p>
     </div>
   );
