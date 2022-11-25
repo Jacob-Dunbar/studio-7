5df48a0766d87f91faac3feb8e3f24e8acc4878c
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-25 15:35:59 +0000

    fix: text align banner text

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = (props) => {
 
   return (
     <div className="flex sm:h-10 h-12 sm:text-sm text-xs sm:uppercase   text-white justify-start sm:justify-center items-center w-full sm:opacity-80 bg-[#e4816b] ">
-      <h1 className="w-[95%] px-4  sm:w-full sm:px-0">
+      <h1 className="w-[95%] px-4  sm:w-full  sm:text-center ">
         {`Claim your free limited edition water bottle after your first
         class - only in ${date.toLocaleDateString("en-GB", options)}`}
       </h1>
